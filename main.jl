22516675d090b3088e330c744278f937adcee9e2
Changhao Fang <colonfang@outlook.com>
2020-11-23 10:26:31 +0800

    add main.jl, poissonNeymann.jl

--- main.jl
+++ main.jl
@@ -1,50 +1,52 @@
-# # testing of Poisson classical interval
-# length_of_list = 1000000
-# list_test = ones(length_of_list, 2)
-# mulist = range(0, 15, length = length_of_list)
-# for i in 1:length_of_list
-#     func = poissonBG(Float64(mulist[i]), 3.0)
-#     list_test[i, 1] = searchIntervalEdge(func, 0.9)[1]
-#     list_test[i, 2] = searchIntervalEdge(func, 0.9)[2]
-# end
-# pic1 = plot(list_test[:,1], mulist, show = true)
-# plot!(list_test[:,2], mulist)
-# savefig(pic1, "pic1.png")
-# upper_list = ones(length_of_list)
-# for i in 1:length_of_list
-#     func = poissonBG(Float64(mulist[i]), 3.0)
-#     upper_list[i] = searchUpperEdge(func, 0.9)
-#     # print(searchUpperEdge(func, 0.9))
-# end
-# pic2 = plot(upper_list, mulist, show = true)
-# savefig(pic2, "pic2.png")
-# func = poissonBG(0.5, 3.0)
-# for i in 0:10
-#     print(pdf(func, i), "\n")
-# end
 push!(LOAD_PATH, ".")
-using PoissonLikelihoodRatio
-using GR
+using PoissonLikelihoodRatio, Tools # Top level packages
+using Plots
+# using GR
 
-# set parameters
-length_of_list = 10000 # parameter used in [S0556-2821(98)00109-X] is 10,000.
-mu_upper_limit = 50
-bg = 3.0
-cfdent_level = 0.97
-n0_upper = 100
+mu_list = 0:0.005:50
+n0_limit_list = constructBelt(2.5)
 
-mu_list = range(0, mu_upper_limit, length = length_of_list)
-n0_limit_list = ones(length_of_list, 2)
-for i in 1:length_of_list
-    n0_limit_list[i, :] = likelihoodRatio(mu_list[i], bg, cfdent_level, n0_upper)
-end
-pic1 = plot(n0_limit_list[:,1], mu_list, n0_limit_list[:,2], mu_list)
+
+# scanBKGGif(2,4)
+# bkgScanGif(4,6)
+# bkgScanGif(20,22)
 
 dict_test = selectMuRegion(mu_list, n0_limit_list)
-for i in 0:20
-    println(i, "\t",dict_test[i])
+
+# for i in 0:30
+#     println(i, "\t",dict_test[i])
+# end
+# plot(n0_limit_list, mu_list, label = ["upper" "lower"], xlim = [0,15], ylim = [0,15]) # for plot
+# plot(n0_limit_list[:,1], mu_list, n0_limit_list[:,2], mu_list, xlim = [0,15], ylim = [0,15]) # for GR
+
+# plot uppper limit for n = 0
+bkg_scan = 0:0.01:25
+upper_mu_for_0 = ones(length(bkg_scan))
+upper_mu_for_1 = ones(length(bkg_scan))
+upper_mu_for_2 = ones(length(bkg_scan))
+upper_mu_for_3 = ones(length(bkg_scan))
+upper_mu_for_4 = ones(length(bkg_scan))
+for i in 1:length(bkg_scan)
+    n0_limit_list = constructBelt(bkg_scan[i])
+    upper_mu_dict = selectMuRegion(mu_list, n0_limit_list)
+    upper_mu_for_0[i] = upper_mu_dict[0]
+    upper_mu_for_1[i] = upper_mu_dict[1]
+    upper_mu_for_2[i] = upper_mu_dict[2]
+    upper_mu_for_3[i] = upper_mu_dict[3]
+    upper_mu_for_4[i] = upper_mu_dict[4]
 end
+fig = plot(bkg_scan, upper_mu_for_0)
+plot!(bkg_scan, upper_mu_for_1)
+plot!(bkg_scan, upper_mu_for_2)
+plot!(bkg_scan, upper_mu_for_3)
+plot!(bkg_scan, upper_mu_for_4)
+savefig(fig, "mu2_bkg.png")
+
+
+# showSortedResult(0.9800, 5.0, 60)
 
 # for i in 1:200
 #     println(n0_limit_list[i,1], "\t",n0_limit_list[i,2], "\t",mu_list[i])
 # end
+
+# Caculate mu_2 as function of bkg.
