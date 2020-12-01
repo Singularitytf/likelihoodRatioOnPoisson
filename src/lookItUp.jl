8b28d0a3031a7fc5e6c291f89abc45a0a9336724
Changhao Fang <colonfang@outlook.com>
2020-12-01 09:54:11 +0800

    added findInterval function, and been tested well

--- src/lookItUp.jl
+++ src/lookItUp.jl
@@ -1,17 +1,16 @@
 using JLD
 using Plots
 
-mu2_dic = load("../meta_data/mu_bkg_dict_0.9.jld")
-mu1_dic = 0
-
-function getStepIndex(list, bkg_scan_precision)
+function getStepIndex(list)
     # input an dict mapping n0 to mu_2(b). output its step's index
     
     step_index_list = []
+    bkg_scan_precision = abs(list[2] - list[1])
+    # println(bkg_scan_precision)
 
     len = length(list)
     for i in 1:len-1
-        if (list[i+1] - list[i])/bkg_scan_precision > 20 # slightly larger than mu precision. If mu_2 is non-continous, it should larger than mu_precision.
+        if (list[i+1] - list[i])/bkg_scan_precision > 15 # slightly larger than mu precision. If mu_2 is non-continous, it should larger than mu_precision.
             push!(step_index_list, i+1) # push peak index.
         end
     end
@@ -20,6 +19,7 @@ end
 
 function findIndex(bkg, bkg_scan)
     percision = bkg_scan[2] - bkg_scan[1]
+    index::Int = 0
     for i in 1:length(bkg_scan)
         if abs(bkg - bkg_scan[i]) < percision/2 # bkg_scan precision
             index = i
@@ -37,14 +37,17 @@ function findModifiedUpperLimit(n0::Int, bkg::Float64, mu2_dic, step_index)
     bkg_step_points = bkg_scan[step_index]
     mu2_step_points = mu2_list[step_index]
     index::Int = findIndex(bkg, bkg_scan)
-    print(index)
+    println("--------------------------")
     println("The nearest bkg is ", bkg_scan[index], ", whose mu2 is ", mu2_list[index], ".")
+    println("--------------------------")
     for i in 1:length(step_index)-1
         # find nearest bkg in the dictionary.
         if bkg > bkg_step_points[i] && bkg < bkg_step_points[i+1]
+            println("The bkg interval is:")
             println(bkg_step_points[i], "\t", bkg_step_points[i+1])
             # if mu is in the dip reigon, modified it!
             if mu2_list[index] <= mu2_step_points[i+1]
+                println("The mu interval is:")
                 println(mu2_list[index], "\t", mu2_step_points[i+1])
                 return mu2_step_points[i+1]
             else
@@ -55,12 +58,26 @@ function findModifiedUpperLimit(n0::Int, bkg::Float64, mu2_dic, step_index)
 end
 
 function findLowerLimit(n0, bkg, mu1_dic)
-
-
+    bkg_scan = mu1_dic["bkg_scan"]
+    mu1_list = mu1_dic[string(n0)]
+    index::Int = findIndex(bkg, bkg_scan)
+    println("--------------------------")
+    println("The nearest bkg is ", bkg_scan[index], ", whose mu2 is ", mu1_list[index], ".")
+    println("--------------------------")
+    return mu1_list[index]
 end
 
-bkg_scan = 0:0.001:25
-step_index = getStepIndex(mu2_dic["0"], 0.001)
-plot(bkg_scan, mu2_dic["0"])
-scatter!(bkg_scan[mu2_step_index], mu2_dic["0"][mu2_step_index])
-findModifiedUpperLimit(0, 5.0, bkg_scan, mu2_dic, step_index)
+function findInterval(n0, bkg)
+    step_index = getStepIndex(mu2_dic[string(n0)])
+    mu2 = findModifiedUpperLimit(n0, bkg, mu2_dic, step_index)
+    mu1 = findLowerLimit(n0, bkg, mu1_dic)
+    return [mu1, mu2]
+end
+
+global mu1_dic = load("mu_bkg_data/mu1_bkg_dict_50_20_0.001.jld")
+global mu2_dic = load("mu_bkg_data/mu2_bkg_dict_50_20_0.001.jld")
+bkg_scan = mu2_dic["bkg_scan"]
+step_index = getStepIndex(mu2_dic["2"])
+plot(bkg_scan, mu2_dic["2"])
+scatter!(bkg_scan[step_index], mu2_dic["2"][step_index])
+# print("μ interval is:", findInterval(0, 3.0))
